f5a0d98d27da67aca2b3f60b7f1680357e827caf
Imre Kószó <imrekoszo@users.noreply.github.com>
2023-12-05 18:16:59 +0100

    refactor 23d3 to single pass transjuxt

--- src/imrekoszo/advent/y23/day3.clj
+++ src/imrekoszo/advent/y23/day3.clj
@@ -7,54 +7,55 @@
 
 (set! *warn-on-reflection* true)
 
-(defn match-coords
-  "Returns the coordinates of the first character of the current match of matcher occupies.
-
-  Coordinate format is [row-index character-position]"
-  [row-idx ^Matcher matcher]
-  [row-idx (.start matcher)])
-
-(defn match-adjacent
+(defn adjacent-coords
   "Returns the set of coordinates surrounding the current match of matcher.
 
   Coordinate format is [row-index character-position]"
   [row-idx ^Matcher matcher]
   (let [s (.start matcher)
         e (.end matcher)
-        c-range (set (range s e))
-        c-around (range (dec s) (inc e))]
+        char-range (set (range s e))
+        char-around (range (dec s) (inc e))]
     (x/into #{}
-      (x/for [c _ r (range (dec row-idx) (+ 2 row-idx))
-              :when (not (and (= row-idx r) (c-range c)))]
+      (x/for
+        [c _ r (range (dec row-idx) (+ 2 row-idx))
+         :when (not (and (= row-idx r) (char-range c)))]
         [r c])
-      c-around)))
+      char-around)))
 
-(defn token-data-fn
-  "Returns a function to be used with map-indexed and which returns an
-  iterable of token information found in a row.
+(defn parse-row
+  "Returns token information about a row.
+
+  {:numbers [{:value 123 :adjacent #{[1 2] [1 3] ...}} ...]
+   :syms [{:value \"=\" :coords [2 5]} ...]}
 
   Tokens are either numbers or symbols matching sym-re.
-  Return format is [:kw-of-type info]
-  Info is a map of :value and :adjacent coords for numbers,
-  and simply coords for symbols"
-  [sym-re]
-  (fn [row-idx row]
-    (let [!matcher (re-matcher (re-pattern (str sym-re "|\\d+")) row)]
+  For numbers, we return their value and adjacent coordinates.
+  For symbols, we return their value and coordinates."
+  [row-idx row]
+  (x/into {}
+    (x/by-key
+      #(if (:sym %) :syms :nums)
+      (x/into []))
+    (let [matcher (re-matcher #"([^.\d])|(?:\d+)" row)]
       (iteration
-        (fn [_] (re-find !matcher))
-        :vf #(if (re-find #"\d" %)
-               [:number
-                {:value (parse-long %)
-                 :adjacent (match-adjacent row-idx !matcher)}]
-               [:sym-coords
-                (match-coords row-idx !matcher)])))))
+        (fn [_] (re-find matcher))
+        :vf (fn [[token sym]]
+              (if (some? sym)
+                {:sym token
+                 :coords [row-idx (.start matcher)]}
+                {:value (parse-long token)
+                 :adjacent (adjacent-coords row-idx matcher)}))))))
 
 (defn numbers-next-to-symbols
   "Looks at data from 3 adjacent rows and returns an eduction of the numbers
    from the middle row that are adjacent to a symbol on any row"
   [three-rows]
-  (x/for [{:keys [value adjacent]} (:number (second three-rows))
-          :when (x/some (keep #(some adjacent (:sym-coords %))) three-rows)]
+  (x/for
+    [{:keys [value adjacent]} (:nums (second three-rows))
+     :when (x/some
+             (comp (mapcat :syms) (keep #(adjacent (:coords %))))
+             three-rows)]
     value))
 
 (defn product-next-to-symbols-next-to-2-numbers
@@ -63,42 +64,34 @@
   row, which is next to exactly 2 numbers"
   [three-rows]
   (x/for
-    [sym-coord (:sym-coords (second three-rows))
-     :let [number-values
-           (x/into []
-             (comp
-               (mapcat :number)
-               (filter some?)
-               (filter #((:adjacent %) sym-coord))
-               (map :value)
-               (take 3))
-             three-rows)]
-     :when (= 2 (count number-values))]
-    (apply * number-values)))
+    [sym (:syms (second three-rows))
+     :when (= "*" (:sym sym))
+     :let [sym-coord (:coords sym)
+           values (x/into []
+                    (comp
+                      (mapcat :nums)
+                      (filter (every-pred some? #((:adjacent %) sym-coord)))
+                      (map :value)
+                      (take 3))
+                    three-rows)]
+     :when (= 2 (count values))]
+    (apply * values)))
 
-(defn calculate-sum [input sym-re three-rows->numbers]
-  (transduce
+(defn solve [input]
+  (x/some
     (comp
       cat
-      (map-indexed (token-data-fn sym-re))
-      (map #(x/into {} (x/by-key (x/into [])) %))
+      (map-indexed parse-row)
       (x/partition 3 1)
-      (mapcat three-rows->numbers))
-    + 0
+      (x/transjuxt
+        {:part1 (comp (mapcat numbers-next-to-symbols) (x/reduce +))
+         :part2 (comp (mapcat product-next-to-symbols-next-to-2-numbers) (x/reduce +))}))
     [[""] input [""]]))
-
-(defn part1 [input]
-  (calculate-sum input #"[^.\d]" numbers-next-to-symbols))
-
-(defn part2 [input]
-  (calculate-sum input #"\*" product-next-to-symbols-next-to-2-numbers))
 
 (comment
 
-  (= 4361 (part1 (util/input-seq "data/y23/day3/test.txt")))
-  (part1 (util/input-seq "data/y23/day3/input.txt")) ;; 514969
+  (= (solve (util/input-seq "data/y23/day3/test.txt"))
+    {:part1 4361, :part2 467835})
 
-  (= 467835 (part2 (util/input-seq "data/y23/day3/test.txt")))
-  (part2 (util/input-seq "data/y23/day3/input.txt")) ;; 78915902
-
+  (solve (util/input-seq "data/y23/day3/input.txt")) ;; {:part1 514969, :part2 78915902}
   )
